b7247adcc6ce4564ff762780b5be99d9c67e38eb
Tcroneb Hackx <cartoonboyzfamily@gmail.com>
2024-08-23 00:39:08 +0200

    Update qrr.js

--- qrr.js
+++ qrr.js
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *MR•ONLINE-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/TcronebHackx-Md_
+❒ *Author:* _wa.me/263716394979_
+❒ *YT:* _https://youtube.com/@cbf_263_
 └────────────⭓
 `
 
